f5ef6d5388fd9d55c2a646e8583c50d6a8d58c8d
Alexander Laptev <laptev@speechpro.com>
2020-08-28 12:58:28 +0300

    ctc-crf installation fix

--- tools/installers/install_ctc-crf.sh
+++ tools/installers/install_ctc-crf.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-
 if [ $# != 0 ]; then
     echo "Usage: $0"
     exit 1;
 fi
+
+kld_dir=$(pwd)/kaldi
 
 # TODO(kamo): Consider clang case
 # Note: Requires gcc>=4.9.2 to build extensions with pytorch>=1.0
@@ -18,20 +19,19 @@ git clone https://github.com/thu-spmi/CAT.git
 
 (
     set -euo pipefail
-    cd CAT
+    cd CAT && git checkout 5c449fb
     (
         set -euo pipefail
-        cp -f src/kaldi-patch/latgen-faster.cc kaldi/src/bin
-        cd kaldi/src/bin
+        [ -d "${kld_dir}" ] && cp -f src/kaldi-patch/latgen-faster.cc ${kld_dir}/src/bin
+        cd ${kld_dir}/src/bin
         sed -i "s:BINFILES =:BINFILES = latgen-faster:g" Makefile
         make latgen-faster
-        cd ../../../
     )
 
     cd src/ctc_crf
     (
         set -euo pipefail
-        OPENFST=../../kaldi/tools/openfst make GPUCTC GPUDEN PATHWEIGHT
+        [ -d "${kld_dir}/tools/openfst" ] && OPENFST=${kld_dir}/tools/openfst make GPUCTC GPUDEN PATHWEIGHT
     )
 
     (
