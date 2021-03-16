23f9bc459bdc2f1ffddcfe3d1532b76c0c5f184d
Andrusenko Andrey <andrusenko@speechpro.com>
2021-03-16 15:36:18 +0300

    add conv_filters param for conv2d subsampling layer

--- espnet2/asr/encoder/transformer_encoder.py
+++ espnet2/asr/encoder/transformer_encoder.py
@@ -71,6 +71,7 @@ class TransformerEncoder(AbsEncoder):
         positional_dropout_rate: float = 0.1,
         attention_dropout_rate: float = 0.0,
         input_layer: Optional[str] = "conv2d",
+        conv_filters=None,
         pos_enc_class=PositionalEncoding,
         normalize_before: bool = True,
         concat_after: bool = False,
@@ -95,7 +96,7 @@ class TransformerEncoder(AbsEncoder):
                 pos_enc_class(output_size, positional_dropout_rate),
             )
         elif input_layer == "conv2d":
-            self.embed = Conv2dSubsampling(input_size, output_size, dropout_rate)
+            self.embed = Conv2dSubsampling(input_size, conv_filters, output_size, dropout_rate)
         elif input_layer == "conv2d6":
             self.embed = Conv2dSubsampling6(input_size, output_size, dropout_rate)
         elif input_layer == "conv2d8":
