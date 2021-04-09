337f0e0f242bfe1a80df6595337f225981c47ca0
andrusenko <andrusenkoau@speechpro.com>
2021-04-09 16:47:51 +0300

    add ingnore_nan_grad to ctc-crf loss

--- espnet2/asr/ctc_crf.py
+++ espnet2/asr/ctc_crf.py
@@ -7,9 +7,10 @@ Based on https://github.com/thu-spmi/CAT/
 """
 
 import torch
+import logging
 
 import ctc_crf_base
-
+import warpctc_pytorch as warp_ctc
 
 def _assert_no_grad(tensor):
     assert not tensor.requires_grad, "shouldn't require grads"
@@ -72,6 +73,8 @@ class CTC_CRF_LOSS(torch.nn.Module):
         super(CTC_CRF_LOSS, self).__init__()
         self.crf = _CRF.apply
         self.ctc = torch.nn.CTCLoss(reduction="none")
+        self.ignore_nan_grad = True
+        #self.ctc = warp_ctc.CTCLoss(size_average=False, reduce=False)
         self.lamb = lamb
         self.size_average = size_average
         self.reduce = reduce
@@ -92,17 +95,72 @@ class CTC_CRF_LOSS(torch.nn.Module):
         _assert_no_grad(input_lengths)
         _assert_no_grad(label_lengths)
 
-        # crf only supports float32
-        crf_cost = self.crf(
-            logits,
-            labels,
-            input_lengths,
-            label_lengths,
-            self.size_average,
-        )
         ctc_cost = self.ctc(logits, labels, input_lengths, label_lengths)
+        nan_grad = False
+        if ctc_cost.requires_grad and self.ignore_nan_grad:
+            # ctc_grad: (L, B, O)
+            ctc_grad = ctc_cost.grad_fn(torch.ones_like(ctc_cost))
+            ctc_grad = ctc_grad.sum([0, 2])
+            indices = torch.isfinite(ctc_grad)
+            size = indices.long().sum()
+            if size == 0:
+                # Return as is
+                logging.warning(
+                    "All samples in this mini-batch got nan grad."
+                    " Returning nan value instead of CTC loss"
+                )
+            elif size != logits.size(1):
+                logging.warning(
+                    f"{logits.size(1) - size}/{logits.size(1)}"
+                    " samples got nan grad."
+                    " These were ignored for CTC loss."
+                )
+
+                # Create mask for target
+                target_mask = torch.full(
+                    [labels.size(0)],
+                    1,
+                    dtype=torch.bool,
+                    device=labels.device,
+                )
+                s = 0
+                for ind, le in enumerate(label_lengths):
+                    if not indices[ind]:
+                        target_mask[s : s + le] = 0
+                    s += le
+
+                # Calc loss again using maksed data
+                ctc_cost = self.ctc(
+                    logits[:, indices, :],
+                    labels[target_mask],
+                    input_lengths[indices],
+                    label_lengths[indices],
+                )
+                nan_grad =True
+        else:
+            size = logits.size(1)
+
+        # crf only supports float32
+        if not nan_grad:
+            crf_cost = self.crf(
+                logits,
+                labels,
+                input_lengths,
+                label_lengths,
+                self.size_average,
+            )
+        else:
+            crf_cost = self.crf(
+                logits[:, indices, :],
+                labels[target_mask],
+                input_lengths[indices],
+                label_lengths[indices],
+                self.size_average,
+            )
+        #print("[DEBUG]: I'm here...")
+        #ctc_cost = self.ctc(logits.to(dtype=torch.float32), labels.cpu().int(), input_lengths.cpu().int(), label_lengths.cpu().int())
         if self.size_average:
-            ctc_cost /= logits.size(1)
+            ctc_cost /= size
         cost = crf_cost + (1 + self.lamb) * ctc_cost
         if self.reduce:
             cost = cost.sum()
